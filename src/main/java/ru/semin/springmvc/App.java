dfa6662ce7c266700a3194089e82f8d9eeaab3c4
Ilya5800 <ilyasemin870@gmail.com>
2025-07-26 12:54:49 +0300

    Первое приложение Hibernate Один ко многим

--- src/main/java/ru/semin/springmvc/App.java
+++ src/main/java/ru/semin/springmvc/App.java
@@ -3,8 +3,11 @@ package ru.semin.springmvc;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import ru.semin.springmvc.model.Item;
 import ru.semin.springmvc.model.Person;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -12,16 +15,19 @@ import java.util.List;
  */
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
 
         try {
             session.beginTransaction();
-     session.createQuery("delete from Person  where age<30").executeUpdate();
+            Person person = session.find(Person.class, 4);
+           Item item = session.find(Item.class, 1);
+           item.getOwner().getItems().remove(item);
 
-
-session.getTransaction().commit();
+           item.setOwner(person);
+           person.getItems().add(item);
+            session.getTransaction().commit();
         } finally {
             sessionFactory.close();
         }
