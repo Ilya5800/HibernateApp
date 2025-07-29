e4baa1e70882b4e7552a891b2183a9c493ced697
Ilya5800 <ilyasemin870@gmail.com>
2025-07-30 00:36:10 +0300

    Первое приложение Hibernate Lazy loading

--- src/main/java/ru/semin/springmvc/App.java
+++ src/main/java/ru/semin/springmvc/App.java
@@ -1,39 +1,42 @@
 package ru.semin.springmvc;
 
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.mapping.List;
-import ru.semin.springmvc.model.Actor;
-import ru.semin.springmvc.model.Movie;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import ru.semin.springmvc.model.Item;
+import ru.semin.springmvc.model.Person;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Actor.class).addAnnotatedClass(Movie.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
 
         try {
             session.beginTransaction();
-
-
-            Actor actor=session.find(Actor.class,2);
-            System.out.println(actor.getMovieList());
-
-            Movie movieToRemove = actor.getMovieList().get(0);
-            actor.getMovieList().remove(0);
-            movieToRemove.getActors().remove(actor);
-
-
+            Person person= session.find(Person.class, 1);
+            System.out.println("Получили человека из таблицы");
 
             session.getTransaction().commit();
+
+            session =sessionFactory.getCurrentSession();
+            session.beginTransaction();
+
+            System.out.println("Находимся внутри второй транзакции");
+
+            person=(Person) session.merge(person);
+            session.createQuery("select i from Item i where i.owner.id=:personID", Item.class)
+                    .setParameter("personID", person.getId()).getResultList();
+            Hibernate.initialize(person.getItems());
+            session.getTransaction().commit();
+            System.out.println("Вне сессии");
+            System.out.println(person.getItems());
+
+
         } finally {
             sessionFactory.close();
         }
